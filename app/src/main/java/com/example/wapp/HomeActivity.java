ec5ff23df086c5b3557cf43beb117422048dfa10
manasgupta01 <manasgupta7624@gmail.com>
2023-06-13 15:10:13 +0530

    add button

--- app/src/main/java/com/example/wapp/HomeActivity.java
+++ app/src/main/java/com/example/wapp/HomeActivity.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,6 +24,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
@@ -57,14 +59,20 @@ public class HomeActivity extends AppCompatActivity {
             if (task.isSuccessful()) {
                 itemList.clear();
                 for (QueryDocumentSnapshot document : task.getResult()) {
+
                     String name = document.getString("name");
                     double price = document.getDouble("price");
-                    int quantity = document.getLong("quantity").intValue();
-                    byte[] imageBytes = document.get("imageBytes", byte[].class);
+                    int quantity =0;
+                    String imageString = document.getString("image");
 
+                    // Convert the Base64 string to a byte array
+                    byte[] imageBytes = Base64.decode(imageString, Base64.DEFAULT);
+
+                    Log.d(TAG, "Fetched Image: " + Arrays.toString(imageBytes));
                     Item item = new Item(name, price, quantity, imageBytes);
                     itemList.add(item);
                     Log.d(TAG, "Fetched Item: " + item.toString());
+                    Log.d(TAG, "Fetched Image: " + Arrays.toString(item.getImageBytes()));
                 }
                 itemAdapter.notifyDataSetChanged();
             } else {
--- app/src/main/java/com/example/wapp/adapter/Item.java
+++ app/src/main/java/com/example/wapp/adapter/Item.java
@@ -9,7 +9,7 @@ public class Item {
     public Item(String name, double price, int quantity, byte[] imageBytes) {
         this.name = name;
         this.price = price;
-        this.quantity = quantity;
+        this.quantity = 0;
         this.imageBytes = imageBytes;
     }
 
