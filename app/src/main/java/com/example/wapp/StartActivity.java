25ba93d9aeb54142d53de447bb729b3d0916b8bd
Varun Sen <varunvsrn@gmail.com>
2023-06-22 01:00:08 +0530

    added start screen and auth activity modified

---
+++ app/src/main/java/com/example/wapp/StartActivity.java
@@ -0,0 +1,18 @@
+package com.example.wapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        startActivity(new Intent(getApplicationContext(), AuthenticationActivity.class) );
+        finish();
+    }
+}
