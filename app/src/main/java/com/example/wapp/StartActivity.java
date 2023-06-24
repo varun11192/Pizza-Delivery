5878417589eed112bec507aac803cf52b8d80174
Varun Sen <varunvsrn@gmail.com>
2023-06-24 16:10:31 +0530

    splash screen added

--- app/src/main/java/com/example/wapp/StartActivity.java
+++ app/src/main/java/com/example/wapp/StartActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class StartActivity extends AppCompatActivity {
 
     Button startBtn;
@@ -15,6 +18,14 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+//             User is already authenticated, navigate to the next activity
+            Intent intent = new Intent(StartActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
         startBtn = findViewById(R.id.startButton);
 
--- app/src/main/java/com/example/wapp/adapter/SplashActivity.java
+++ app/src/main/java/com/example/wapp/adapter/SplashActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 import com.example.wapp.AuthenticationActivity;
 import com.example.wapp.HomeActivity;
 import com.example.wapp.R;
+import com.example.wapp.StartActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -15,6 +18,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-         startActivity(new Intent(this, HomeActivity.class));
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), StartActivity.class));
+                finish();
+            }
+        }, 2500);
     }
 }
